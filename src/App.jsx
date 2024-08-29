a8269c914864fa75257147fbf8ce6a29b55c4e36
BradleyLegge <bmlegge12@gmail.com>
2024-08-29 12:41:00 -0400

    Fixed Logic between day, week, month. Fixed mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div className="hero-container">
       <Profile />
-      <ActivityInfo />
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,36 +7,38 @@ const Profile = () => {
   const [timePeriod, setTimePeriod] = useState("weekly");
 
   return (
-    <div className="profile-container">
-      <div className="profile-top">
-        <img className="avatar-img" src={avatar} alt="Image of Jeremy" />
-        <div className="name-info">
-          <p className="report">Report for</p>
-          <h1>Jeremy Robson</h1>
+    <>
+      <div className="profile-container">
+        <div className="profile-top">
+          <img className="avatar-img" src={avatar} alt="Image of Jeremy" />
+          <div className="name-info">
+            <p className="report">Report for</p>
+            <h1>Jeremy Robson</h1>
+          </div>
+        </div>
+        <div className="profile-bottom">
+          <p
+            onClick={() => setTimePeriod("daily")}
+            className={timePeriod === "daily" ? "current-target" : "you"}
+          >
+            Daily
+          </p>
+          <p
+            onClick={() => setTimePeriod("weekly")}
+            className={timePeriod === "weekly" ? "current-target" : "you"}
+          >
+            Weekly
+          </p>
+          <p
+            onClick={() => setTimePeriod("monthly")}
+            className={timePeriod === "monthly" ? "current-target" : "you"}
+          >
+            Monthly
+          </p>
         </div>
       </div>
-
-      <div className="profile-bottom">
-        <p
-          onClick={() => setTimePeriod("daily")}
-          className={timePeriod === "daily" ? "current-target" : "you"}
-        >
-          Daily
-        </p>
-        <p
-          onClick={() => setTimePeriod("weekly")}
-          className={timePeriod === "weekly" ? "current-target" : "you"}
-        >
-          Weekly
-        </p>
-        <p
-          onClick={() => setTimePeriod("monthly")}
-          className={timePeriod === "monthly" ? "current-target" : "you"}
-        >
-          Monthly
-        </p>
-      </div>
-    </div>
+      <ActivityInfo timerPeriod={timePeriod} />;
+    </>
   );
 };
 
